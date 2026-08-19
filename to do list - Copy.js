d1aacd707264c75274497b9ef8596e4bb34315d8
malakelawady <maloukaelawady@gmail.com>
2026-08-19 22:58:06 +0300

    Delete to do list - Copy.js

--- to do list - Copy.js
+++
@@ -1,9 +0,0 @@
-function addTask(){
-    let input = document.getElementById('input');
-    let list = document.getElementById('list');
-    if (input.value==="")return;
-    let li = document.createElement('li');
-    li.innerHTML = input.value+'<button onclick="this.parentElement.remove()">Delete</button>';
-    list.appendChild(li);
-    input.vallue="";
-}
